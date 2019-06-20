f59c17c5742439bb2cbda3224d976fa3966803a0
tavisca-sjana <sjana@tavisca.in>
2019-06-20 07:45:25 -0700

    Added code

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,7 +23,79 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            string [] num1 = equation.Split('*');
+            string [] num2 = num1[1].Split('=');
+            string num3 = num2[1];
+            var operand1 = 0;
+            var operand2 = 0;
+            var result = 0;
+            var temp = 0;
+            
+            if(num1[0].Contains("?"))
+            {
+                operand2 = Int32.Parse(num2[0]);
+                result = Int32.Parse(num3);
+                temp = result/operand2;
+            
+                if(result%operand2 == 0)
+                    return FindMissingDigit(num1[0],temp);
+                return -1;
+            }
+
+            else if(num2[0].Contains("?"))
+            {
+                operand1 = Int32.Parse(num1[0]);
+                result = Int32.Parse(num3);
+                temp = result/operand1;
+            
+                if(result%operand1 == 0)
+                    return FindMissingDigit(num2[0],temp);
+                return -1;
+            }
+
+            else
+            {
+                operand1 = Int32.Parse(num1[0]);
+                operand2 = Int32.Parse(num2[0]);
+                temp = operand1*operand2;
+                
+                return FindMissingDigit(num3,temp);
+            }
+        }
+
+        //Function to find the missing corresponding digit from the number
+        public static int FindMissingDigit(string num,int temp)
+        {
+            //Locates the position of ? in string
+            int positionInString = 0;
+            //Keeps track the position corresponding to ? of digit 
+            int positionInNumber = 0;
+            int rem;
+            foreach(var letter in num)
+            {
+                if(letter == '?')
+                {
+                    break;
+                }
+                positionInString += 1;
+            }
+
+            //Find the position of ? from the end of string
+            positionInString = num.Length-1-positionInString;
+
+
+            while(temp>0)
+            {
+                rem = temp%10;
+                if(positionInString == positionInNumber)
+                {
+                    return rem;
+                }
+                positionInNumber += 1;
+                temp /= 10;
+            }
+            return -1;
+
         }
     }
 }
