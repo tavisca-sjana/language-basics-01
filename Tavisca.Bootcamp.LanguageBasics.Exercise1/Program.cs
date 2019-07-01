03e8259f21cb6f073167e25f18d7aee998f7d858
tavisca-sjana <sjana@tavisca.in>
2019-07-01 08:35:17 -0700

    Refactored variable and method names

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,53 +23,53 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            string [] num1 = equation.Split('*');
-            string [] num2 = num1[1].Split('=');
-            string num3 = num2[1];
+            string firstOperand = equation.Split('*')[0];
+            string secondOperand = equation.Split('*')[1].Split('=')[0];
+            string resultOperand = equation.Split('=')[1];
             var operand1 = 0;
             var operand2 = 0;
             var result = 0;
-            var temp = 0;
+            var tempResult = 0;
             
-            if(num1[0].Contains("?"))
+            if(firstOperand.Contains("?"))
             {
-                operand2 = Int32.Parse(num2[0]);
-                result = Int32.Parse(num3);
-                temp = result/operand2;
+                operand2 = Int32.Parse(secondOperand);
+                result = Int32.Parse(resultOperand);
+                tempResult = result/operand2;
             
                 if(result%operand2 == 0)
-                    return FindMissingDigit(num1[0],temp);
+                    return FindPositionOfMissingDigit(firstOperand,tempResult);
                 return -1;
             }
 
-            else if(num2[0].Contains("?"))
+            else if(secondOperand.Contains("?"))
             {
-                operand1 = Int32.Parse(num1[0]);
-                result = Int32.Parse(num3);
-                temp = result/operand1;
+                operand1 = Int32.Parse(firstOperand);
+                result = Int32.Parse(resultOperand);
+                tempResult = result/operand1;
             
                 if(result%operand1 == 0)
-                    return FindMissingDigit(num2[0],temp);
+                    return FindPositionOfMissingDigit(secondOperand,tempResult);
                 return -1;
             }
 
             else
             {
-                operand1 = Int32.Parse(num1[0]);
-                operand2 = Int32.Parse(num2[0]);
-                temp = operand1*operand2;
+                operand1 = Int32.Parse(firstOperand);
+                operand2 = Int32.Parse(secondOperand);
+                tempResult = operand1*operand2;
                 
-                return FindMissingDigit(num3,temp);
+                return FindPositionOfMissingDigit(resultOperand,tempResult);
             }
         }
 
         //Function to find the missing corresponding digit from the number
-        public static int FindMissingDigit(string num,int temp)
+        public static int FindPositionOfMissingDigit(string num,int temp)
         {
             //Locates the position of ? in string
-            int positionInString = 0;
+            int positionOfMissingDigitInString = 0;
             //Keeps track the position corresponding to ? of digit 
-            int positionInNumber = 0;
+            int positionOfMissingDigitInNumber = 0;
             int rem;
             foreach(var letter in num)
             {
@@ -77,21 +77,21 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 {
                     break;
                 }
-                positionInString += 1;
+                positionOfMissingDigitInString += 1;
             }
 
             //Find the position of ? from the end of string
-            positionInString = num.Length-1-positionInString;
+            positionOfMissingDigitInString = num.Length-1-positionOfMissingDigitInString;
 
 
             while(temp>0)
             {
                 rem = temp%10;
-                if(positionInString == positionInNumber)
+                if(positionOfMissingDigitInString == positionOfMissingDigitInNumber)
                 {
                     return rem;
                 }
-                positionInNumber += 1;
+                positionOfMissingDigitInNumber += 1;
                 temp /= 10;
             }
             return -1;
